1e30d9f968032020966bd9f51ed6ba1cbfacb1e7
Michael Sharp <msharp.oh@gmail.com>
2015-11-25 18:17:41 -0500

    changed file structure until i decide on modulation

--- app.js
+++ app.js
@@ -1,7 +1,3 @@
-/*
- * Express-generated with author tweeks.
- */
-
 /* Module Dependencies */
 var express = require("express");
 var path = require("path");
@@ -12,14 +8,14 @@ var parseUrl = require("parseurl");
 var bodyParser = require("body-parser");
 var session = require("express-session");
 
-/* HTTP page routes */
-var routes = require("./routes/routes");
-
 /* Express Application */
 var app = express();
 
+/* HTTP page routes */
+var router = express.Router();
+
 /*Express-Session session setup */
-express().use(session({
+app.use(session({
   name: "max-vote0.0.0",
   secret: "m-l-h-93",
   resave: false,
@@ -37,12 +33,39 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
+/* GET home page. */
+router.get("/", function(request, response, next) {
+  response.render("home", { title: "MaxVote | A simple live-polling resource" });
+
+});
+
+/* GET signup page. */
+router.get("/signup", function(request, response, next) {
+  response.render("signup", { title: "MaxVote | Sign Up"});
+
+});
+
+/* GET login page. */
+router.get("/login", function(request, response, next) {
+  response.render("login", { title: "MaxVote | Log in"});
+});
+
+/* GET dashboard page. */
+router.get("/dashboard", function(request, response, next) {
+  response.render("dashboard", { title: "MaxVote | Dashboard"});
+});
+
+/* GET settings page. */
+router.get("/settings", function(request, response, next) {
+  response.render("settings", { title: "MaxVote | Settings"});
+});
+
 /* HTTP page routing */
-app.use("/", routes);
-app.use("/signup", routes);
-app.use("/login", routes);
-app.use("/settings", routes);
-app.use("/dashboard", routes);
+app.use("/", router);
+app.use("/signup", router);
+app.use("/login", router);
+app.use("/settings", router);
+app.use("/dashboard", router);
 
 /* Catch 404 error and forward to error handler */
 app.use(function(request, response, next) {
